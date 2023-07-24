3ee0fc10486c9eb18d9e1792a414afff043199d7
abuh004 <husam.abuhabib@arvato.scs.com>
2023-07-24 18:24:16 +0200

    Add logs for OrderController class

--- starter_code/src/main/java/com/example/demo/controllers/OrderController.java
+++ starter_code/src/main/java/com/example/demo/controllers/OrderController.java
@@ -1,21 +1,16 @@
 package com.example.demo.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.model.persistence.Cart;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.UserOrder;
-import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.OrderRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/order")
@@ -27,16 +22,20 @@ public class OrderController {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+
+	static final Logger log = LoggerFactory.getLogger(OrderController.class);
 	
 	
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.error("order - submit: Trying to submit order for non-existing user");
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		log.info("order - submit: Successfully submit the order for the user '{}'", user.getUsername());
 		return ResponseEntity.ok(order);
 	}
 	
@@ -44,8 +43,10 @@ public class OrderController {
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.error("order - history: Trying to get order history for non-existing user");
 			return ResponseEntity.notFound().build();
 		}
+		log.info("order - history: Successfully return the order history for the user '{}'", user.getUsername());
 		return ResponseEntity.ok(orderRepository.findByUser(user));
 	}
 }
