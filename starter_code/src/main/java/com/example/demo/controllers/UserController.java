5477425faacbc4a0500e583a4f13884b7ac4c62b
abuh004 <husam.abuhabib@arvato.scs.com>
2023-07-24 18:24:34 +0200

    Add logs for UserController class

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,23 +1,16 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.model.persistence.Cart;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -31,6 +24,8 @@ public class UserController {
 
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	static final Logger log = LoggerFactory.getLogger(UserController.class);
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
@@ -48,10 +43,12 @@ public class UserController {
 		String userName = createUserRequest.getUsername();
 		if (userName == null || userName.isEmpty()) {
 			String errorMessage = "The user name can't be null or empty";
+			log.error("create - user - Error - Trying to create a user with null or empty user name");
 			return ResponseEntity.badRequest().body(errorMessage);
 		}
 		if(userRepository.findByUsername(userName) != null){
 			String errorMessage = "The user name is already used";
+			log.error("create - user - Error - Trying to create user with exited user name");
 			return ResponseEntity.badRequest().body(errorMessage);
 		}
 		User user = new User();
@@ -62,10 +59,12 @@ public class UserController {
 		if(createUserRequest.getPassword().length()<7 ||
 				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
 			String errorMessage = "Password must be at least 7 characters long and match the confirmation password.";
+			log.error("create - user - Error - Trying to create user with password less than 7 characters");
 			return ResponseEntity.badRequest().body(errorMessage);
 		}
 		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 		userRepository.save(user);
+		log.info("create - user - Successfully - created new user with user name: '{}'",user.getUsername());
 		return ResponseEntity.ok(user);
 	}
 	
